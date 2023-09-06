275ef8dd064bd90c88d71fa6cbf7d5ac06835373
samadtirmizi7 <144122288+samadtirmizi7@users.noreply.github.com>
2023-09-06 11:48:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+{// CHAPTER 1 (ALERTS)
+
+// Task # 1:
+
+// var firstName = "sam";
+// var lastName = "Tir";
+// var email = "SamTir@example.com";
+// var phoneNumber = "111-222-333";
+// var password = "*********";
+// alert(firstName);
+// alert(lastName);
+// alert(email);
+// alert(phoneNumber);
+// alert(password);
+
+// Task # 2:
+
+// alert("You're learning JavaScript!");
+
+// Task # 3:
+
+// alert("Hello...");
+}
+
+
+{// CHAPTER 2 (VARIABLES FOR STRING)
+
+// Task # 1:
+
+// var variableName = "this is camelCase element";
+
+// Task # 2:
+
+// var varibalesName;
+// varibalesName = "its task 2 of chpter 2 JS"
+
+// Task # 3: 
+ 
+// var teamName = "JS team" 
+// alert(teamName)
+
+// Task # 4:
+
+// var bestMan = "Charlie";
+// bestMan = "James"
+}
+
+
+{// CHAPTER # 3 (VARIABLES FOR NUMBERS)
+
+// Task # 1:
+
+var caseQty = 10;
+console.log(caseQty)
+
+// Task # 2:
+
+var caseQty = 144;
+console.log(caseQty)
+
+// Task # 3:
+
+var num = 9;
+result = num + 5;
+console.log(result)
+
+// Task # 4:
+
+var sumNumber;
+sumNumber = 9 + 9;
+console.log(sumNumber)
+
+// Task # 5:
+
+var carPrice = 90000;
+var deliveryCharge = 10000;
+var totalPrice = carPrice + deliveryCharge;
+console.log(totalPrice)
+
+// Task # 6:
+
+var vaRiAble = 15;
+vaRiAble = ++vaRiAble;
+console.log(vaRiAble)
+}
+
+
+{// CHAPTER # 4 (VARIABLES NAMES LEGAL AND ILLEGAL)
+
+// Task # 1:
+
+var productcost = 3.45;
+console.log(productcost)
+
+// Task # 2:
+
+var nameofBand = "task 2";
+console.log(nameofBand)
+
+// Task # 3:
+
+var legalVariable = 99;
+console.log(legalVariable)
+
+// Task # 4:
+
+var firstName = "Syed Abdul Samad"
+var lastName = "Tirmizi"
+console.log(firstName+ " " +lastName)
+
+// Task # 5:
+// Legal Variables
+
+var pakistan00Zindabad;
+var pakistan_Zindabad;
+var $pakistanZindabad;
+var pakistanZindabad00;
+
+//Illegal Variables
+
+// var 123pak; 
+// var ok Pak;
+// var ok-pak;
+}
+
+{// CHAPTER # 4 (MATH EXPRESSION I)
+// Task # 1:
+
+// modulus sign (%)
+
+// Task # 2:
+
+// var num = 20 % 6;
+// answer is 2
+
+// Task # 3:
+
+var largeNum; 
+largeNum = 1000 * 2000;
+console.log(largeNum)
+
+// Task # 4:
+
+var variable1 = 19;
+var variable2 = 9;
+var result = variable1 - variable2;
+console.log(result)
+
+// Task # 5:
+
+var RemaInder = 25 % 12;
+console.log(RemaInder)
+
+// Task # 6:
+
+var num1 = 4;
+var num2 = 7;
+var result = num1 * num2;
+alert(result)
+
+
+}
